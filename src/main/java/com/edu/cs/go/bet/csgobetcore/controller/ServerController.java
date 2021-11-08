34268dcea01d83579192dd48c3dba5a8f4f9d7c0
mtdr <paul.osmolovsky@gmail.com>
2021-11-08 23:34:23 +0300

    CS-000 fix run

--- src/main/java/com/edu/cs/go/bet/csgobetcore/controller/ServerController.java
+++ src/main/java/com/edu/cs/go/bet/csgobetcore/controller/ServerController.java
@@ -5,7 +5,7 @@ import com.edu.cs.go.bet.csgobetcore.dto.server.ServerStatusRequestDto;
 import com.edu.cs.go.bet.csgobetcore.dto.server.ServerStatusResponseDto;
 
 public interface ServerController {
-    String BASE_PATH = "/api/match";
+    String BASE_PATH = "/api/server";
     String SERVER_CREATE = BASE_PATH + "/create";
     String SERVER_START = BASE_PATH + "/start";
     String SERVER_STOP = BASE_PATH + "/stop";
