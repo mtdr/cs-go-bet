44d25fc3034fdd7596078528834a08ffd1f5c606
mtdr <paul.osmolovsky@gmail.com>
2021-11-09 22:26:09 +0300

    CS-003 match service upd

--- src/main/java/com/edu/cs/go/bet/server/service/impl/MatchServiceImpl.java
+++ src/main/java/com/edu/cs/go/bet/server/service/impl/MatchServiceImpl.java
@@ -1,4 +1,14 @@
 package com.edu.cs.go.bet.server.service.impl;
 
-public class MatchServiceImpl {
+import com.edu.cs.go.bet.server.dto.match.CreateMatchRequestDto;
+import com.edu.cs.go.bet.server.dto.match.CreateMatchResponseDto;
+import com.edu.cs.go.bet.server.service.MatchService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatchServiceImpl implements MatchService {
+    @Override
+    public CreateMatchResponseDto create(CreateMatchRequestDto requestDto) {
+        return null;
+    }
 }
