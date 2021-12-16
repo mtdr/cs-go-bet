85ce35c773fa2852aba7ea6d75488da224bbc044
mtdr <paul.osmolovsky@gmail.com>
2021-12-16 18:59:11 +0300

    CS-015 ws module created

---
+++ csgo-bet-ws/src/main/java/com/example/csgobetws/CsgoBetWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.csgobetws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgoBetWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsgoBetWsApplication.class, args);
+    }
+
+}
---
+++ csgo-bet-ws/src/test/java/com/example/csgobetws/CsgoBetWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csgobetws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgoBetWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
