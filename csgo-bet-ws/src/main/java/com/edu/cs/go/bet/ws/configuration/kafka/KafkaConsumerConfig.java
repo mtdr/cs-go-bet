a594c459ea51a5c6108aed11288396516db5bb4a
mtdr <paul.osmolovsky@gmail.com>
2021-12-17 23:54:01 +0300

    CS-015 correct kafka transport

--- csgo-bet-ws/src/main/java/com/edu/cs/go/bet/ws/configuration/kafka/KafkaConsumerConfig.java
+++ csgo-bet-ws/src/main/java/com/edu/cs/go/bet/ws/configuration/kafka/KafkaConsumerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,7 +36,10 @@ public class KafkaConsumerConfig {
                 StringDeserializer.class);
         props.put(
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
+                JsonDeserializer.class);
+        props.put(
+                JsonDeserializer.TRUSTED_PACKAGES,
+                "com.edu.cs.go.bet.api.*");
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
